272890d3f1a6791570a0e6e8b69716cd6bbec4df
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-06-27 22:49:53 +0300

    feebback functions

--- modules/feedbackHandler.js
+++ modules/feedbackHandler.js
@@ -1,2 +1,21 @@
+const Feedbacks = require("../collections/feedbacks");
 
+function feedbackHandler(req, res) {
+  const { userName, usercomment } = req.body;
 
+  Feedbacks.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send("There is an error");
+    } else {
+      data[0].feedbacks.push({
+        name: userName,
+        comment: usercomment,
+      });
+
+      data[0].save();
+      res.send(data[0].feedbacks);
+    }
+  });
+}
+
+module.exports = feedbackHandler;
